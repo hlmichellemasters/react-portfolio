1762dde72d4dc0fd1d3fadf0f16a8b112d7f1330
hlmichellemasters <hlmichellemasters@gmail.com>
2023-07-10 13:16:13 -0500

    add pacman loaders

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoM from '../../assets/images/michelle-M.png';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
 import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -40,43 +41,46 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoM} alt="M" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={firstNameArray}
-            index={15}
-          />
-          <img src={LogoM} alt="M" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={lastNameArray}
-            index={25}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            index={35}
-          />
-        </h1>
-        <h2>
-          Frontend Developer / Python Developer / Machine-Learning Engineer /
-          Biologist
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoM} alt="M" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={firstNameArray}
+              index={15}
+            />
+            <img src={LogoM} alt="M" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={lastNameArray}
+              index={25}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              index={35}
+            />
+          </h1>
+          <h2>
+            Frontend Developer / Python Developer / Machine-Learning Engineer /
+            Biologist
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
