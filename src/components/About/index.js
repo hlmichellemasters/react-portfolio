f5095a4c962afc4dc45e441bfca3f30a4ecd914e
hlmichellemasters <hlmichellemasters@gmail.com>
2023-08-02 09:21:41 -0500

    hide cube spinner on mobile and fix for multiple screen widths

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,6 +15,18 @@ const aboutMeArray = 'About me'.split('');
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleScreenChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    setIsMobile(mediaQuery.matches);
+
+    mediaQuery.addEventListener('change', handleScreenChange);
+  }, []);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -61,28 +73,30 @@ const About = () => {
           </p>
         </div>
 
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        {!isMobile && (
+          <div className="stage-cube-cont">
+            <div className="cubespinner">
+              <div className="face1">
+                <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              </div>
+              <div className="face2">
+                <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              </div>
+              <div className="face3">
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              </div>
+              <div className="face4">
+                <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+              </div>
+              <div className="face5">
+                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+              </div>
+              <div className="face6">
+                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
       <Loader type="pacman" />
     </>
